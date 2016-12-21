3607867e717af21eb9d8905122ec89d32cc58b7d
Alex Mamani Quispe <axelito_b.b@hotmail.com>
2016-12-21 13:45:58 -0500

    Delete types.h

--- types.h
+++
@@ -1,8 +0,0 @@
-typedef unsigned int posicion_x;
-typedef unsigned int posicion_y;
-typedef int corazones;
-typedef int vidas;
-typedef char tecla ;
-typedef char simbolo;
-typedef int direccionales;
-typedef char caracter;
